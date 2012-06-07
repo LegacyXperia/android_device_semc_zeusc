5499e0df2991febe16d9fdc4c369b511761a48fd
freexperia <jerpelea@gmail.com>
2012-06-07 18:34:42 +0300

    zeusc: inital push for zeusc device

---
+++ BoardConfig.mk
@@ -0,0 +1,15 @@
+-include device/semc/zeus-common/BoardConfigCommon.mk
+-include vendor/semc/zeusc/BoardConfigVendor.mk
+
+SENSORS_COMPASS_AK897X := true
+SENSORS_ACCEL_BMA150_INPUT := true
+SENSORS_ACCEL_BMA250_INPUT := false
+SENSORS_PROXIMITY_APDS970X := true
+SENSORS_PRESSURE_BMP180 := false
+
+TARGET_TOUCHPAD_INPUT_DEVICE_ID := 0x10003
+
+BOARD_USES_STEREO_HW_SPEAKER := true
+
+TARGET_OTA_ASSERT_DEVICE := R800x,zeusc
+
---
+++ cm.mk
@@ -0,0 +1,22 @@
+## Specify phone tech before including full_phone
+$(call inherit-product, vendor/cm/config/gsm.mk)
+
+# Release name
+PRODUCT_RELEASE_NAME := R800x
+
+TARGET_BOOTANIMATION_NAME := vertical-480x854
+
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/semc/zeusc/device_zeusc.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := zeusc
+PRODUCT_NAME := cm_zeusc
+PRODUCT_BRAND := SEMC
+PRODUCT_MODEL := Xperia Play
+
+#Set build fingerprint / ID / Product Name ect.
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=R800x BUILD_FINGERPRINT="SEMC/LT18i_0000-0000/LT18i:4.0.3/4.1.C.0.7/-H9_3w:user/release-keys" PRIVATE_BUILD_DESC="LT18i-user 4.0.3 4.1.C.0.7 -H9_3w test-keys"
