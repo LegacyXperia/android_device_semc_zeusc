aa98a7f694236e3519af3ad4b9520471af8acc62
Michael Bestas <mikeioannina@gmail.com>
2012-12-22 21:01:13 +0200

    zeusc: remove prebuilt kernel
    We build kernel from source since long time
    Signed-off-by: Michael Bestas <mikeioannina@gmail.com>
    
    Change-Id: I431a44cb73dec17e8f3e45ef2aeeadf9fbfcc83f

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -3,7 +3,6 @@
 
 TARGET_KERNEL_SOURCE := kernel/semc/msm7x30
 TARGET_KERNEL_CONFIG := cyanogen_zeus_defconfig
-#TARGET_PREBUILT_KERNEL := device/semc/zeusc/prebuilt/kernel
 TARGET_KERNEL_CUSTOM_TOOLCHAIN := arm-eabi-4.4.3
 
 TARGET_RECOVERY_INITRC := device/semc/zeusc/recovery/init.rc
@@ -19,4 +18,3 @@ TARGET_TOUCHPAD_INPUT_DEVICE_ID := 0x10003
 BOARD_USES_STEREO_HW_SPEAKER := true
 
 TARGET_OTA_ASSERT_DEVICE := R800x,zeusc
-
