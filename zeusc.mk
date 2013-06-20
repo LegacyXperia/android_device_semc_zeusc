2bbd103b6409e5c694a4321ff1c470c1e0b82244
Michael Bestas <mikeioannina@gmail.com>
2013-06-20 19:44:38 +0300

    Proprietary side needs to exist

--- zeusc.mk
+++ zeusc.mk
@@ -13,7 +13,7 @@
 # limitations under the License.
 
 # proprietary side of the device
-$(call inherit-product-if-exists, vendor/semc/zeusc/zeusc-vendor.mk)
+$(call inherit-product, vendor/semc/zeusc/zeusc-vendor.mk)
 
 DEVICE_PACKAGE_OVERLAYS += device/semc/zeusc/overlay
 
