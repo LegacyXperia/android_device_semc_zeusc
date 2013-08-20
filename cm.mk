742b4813c21fc4ecb3f5d0c745eb2b67f2cf6779
Michael Bestas <mikeioannina@gmail.com>
2013-08-20 06:07:28 +0300

    zeusc: switch to mini_phone
    Save 13,5MB in /system by excluding sample videos & some ringtones
    
    Signed-off-by: Michael Bestas <mikeioannina@gmail.com>

--- cm.mk
+++ cm.mk
@@ -19,7 +19,18 @@ $(call inherit-product, device/semc/zeusc/full_zeusc.mk)
 $(call inherit-product, vendor/cm/config/cdma.mk)
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/cm/config/common_mini_phone.mk)
+
+# Optional CM packages
+PRODUCT_PACKAGES += \
+    HoloSpiralWallpaper \
+    MagicSmokeWallpapers \
+    NoiseField \
+    Galaxy4 \
+    LiveWallpapers \
+    LiveWallpapersPicker \
+    VisualizationWallpapers \
+    PhaseBeam
 
 # Set build fingerprint / ID / Product Name ect.
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=R800x BUILD_FINGERPRINT="SEMC/R800x_0000-0000/R800x:4.0.3/4.1.H.0.4/-z9-3w:user/release-keys" PRIVATE_BUILD_DESC="R800x-user 4.0.3 4.1.H.0.4 -z9-3w test-keys"
